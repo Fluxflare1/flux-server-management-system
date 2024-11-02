7e33152a5a2294ef6703124314c70a9053d13774
Fluxflare1 <fluxflareltd@gmail.com>
2024-11-02 23:32:33 +0100

    Update urls.py

--- src/backend/app/urls.py
+++ src/backend/app/urls.py
@@ -1,1 +1,14 @@
 
+from django.contrib import admin
+from django.urls import path
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+
+@api_view(["GET"])
+def home(request):
+    return Response({"message": "Flux Server Management Backend Running"})
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', home),
+]
