93c19b014a163591ca2524764af511f6f7fc05f0
Fluxflare1 <fluxflareltd@gmail.com>
2024-11-02 23:31:59 +0100

    Update settings.py

--- src/backend/app/settings.py
+++ src/backend/app/settings.py
@@ -1,1 +1,15 @@
+import os
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': '5432',
+    }
+}
+
+# Allow all hosts for testing; update for production use
+ALLOWED_HOSTS = ["*"]
