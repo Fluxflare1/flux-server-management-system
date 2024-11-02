dfdcc84c611b8dc150526743dccda07906b51de2
Fluxflare1 <fluxflareltd@gmail.com>
2024-11-02 23:34:57 +0100

    Update App.js

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -1,1 +1,21 @@
+import React, { useEffect, useState } from 'react';
 
+function App() {
+    const [message, setMessage] = useState('');
+
+    useEffect(() => {
+        fetch('/')
+            .then((res) => res.json())
+            .then((data) => setMessage(data.message))
+            .catch((err) => console.error(err));
+    }, []);
+
+    return (
+        <div>
+            <h1>Flux Server Management System</h1>
+            <p>{message}</p>
+        </div>
+    );
+}
+
+export default App;
