1449c9d6bf0143401dfffe73e2f93bce9964c755
Fluxflare1 <fluxflareltd@gmail.com>
2024-11-03 00:04:45 +0100

    Create views.py

---
+++ src/backend/app/user_management/user_management/views.py
@@ -0,0 +1,12 @@
+# src/backend/app/user_management/views.py
+from rest_framework import generics
+from .models import User
+from .serializers import UserSerializer
+
+class UserListView(generics.ListCreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class UserDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
