2b1539264b4218dadd570c4915354195296d4a81
Fluxflare1 <fluxflareltd@gmail.com>
2024-11-02 23:58:49 +0100

    Update urls.py

--- src/backend/app/urls.py
+++ src/backend/app/urls.py
@@ -1,4 +1,17 @@
 
+
+# src/backend/app/urls.py
+from django.urls import include, path
+
+urlpatterns = [
+    path("server-management/", include("app.server_management.urls")),
+    path("resource-monitoring/", include("app.resource_monitoring.urls")),
+    path("user-management/", include("app.user_management.urls")),
+    path("version-control/", include("app.version_control.urls")),
+]
+
+
+
 from django.contrib import admin
 from django.urls import path
 from rest_framework.response import Response
