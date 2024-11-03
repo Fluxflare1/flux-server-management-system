ec1b0bdbb5d06f8f1404b630ff5f8a9f435d445d
Fluxflare1 <fluxflareltd@gmail.com>
2024-11-03 21:00:48 +0100

    Update views.py

--- src/backend/app/analytics/views.py
+++ src/backend/app/analytics/views.py
@@ -1,3 +1,19 @@
+
+
+
+from rest_framework import viewsets
+from .models import AnalyticsRecord
+from .serializers import AnalyticsRecordSerializer
+from rest_framework.permissions import IsAuthenticated
+
+class AnalyticsRecordViewSet(viewsets.ModelViewSet):
+    queryset = AnalyticsRecord.objects.all()
+    serializer_class = AnalyticsRecordSerializer
+    permission_classes = [IsAuthenticated]
+
+
+
+
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .analytics_manager import AnalyticsManager
