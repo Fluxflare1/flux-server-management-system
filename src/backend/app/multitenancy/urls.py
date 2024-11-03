8da775ec4f9ac8ce2cbd7c04d60c8f11b25ad75e
Fluxflare1 <fluxflareltd@gmail.com>
2024-11-03 21:11:55 +0100

    Update urls.py

--- src/backend/app/multitenancy/urls.py
+++ src/backend/app/multitenancy/urls.py
@@ -1,20 +1,6 @@
 
 
 
-from rest_framework import viewsets
-from .models import Tenant, TenantResource
-from .serializers import TenantSerializer, TenantResourceSerializer
-from rest_framework.permissions import IsAuthenticated
-
-class TenantViewSet(viewsets.ModelViewSet):
-    queryset = Tenant.objects.all()
-    serializer_class = TenantSerializer
-    permission_classes = [IsAuthenticated]
-
-class TenantResourceViewSet(viewsets.ModelViewSet):
-    queryset = TenantResource.objects.all()
-    serializer_class = TenantResourceSerializer
-    permission_classes = [IsAuthenticated]
 
 
 
