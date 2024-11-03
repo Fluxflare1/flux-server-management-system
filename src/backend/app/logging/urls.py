52e1abd7198e5d295ee1fda91ab276cc597b22fc
Fluxflare1 <fluxflareltd@gmail.com>
2024-11-03 21:16:57 +0100

    Update urls.py

--- src/backend/app/logging/urls.py
+++ src/backend/app/logging/urls.py
@@ -1,3 +1,18 @@
+
+
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import ActivityLogViewSet
+
+router = DefaultRouter()
+router.register(r'activity-logs', ActivityLogViewSet)
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
+
+
+
 from django.urls import path
 from .views import AuditLogView
 
