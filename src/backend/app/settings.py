844642c1b5a30f67903d4ee8aacd602f4ca99f10
Fluxflare1 <fluxflareltd@gmail.com>
2024-11-03 06:45:26 +0100

    Update settings.py

--- src/backend/app/settings.py
+++ src/backend/app/settings.py
@@ -1,4 +1,23 @@
 
+
+
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DATABASE_NAME'),
+        'USER': os.getenv('DATABASE_USER'),
+        'PASSWORD': os.getenv('DATABASE_PASSWORD'),
+        'HOST': os.getenv('DATABASE_HOST'),
+        'PORT': os.getenv('DATABASE_PORT'),
+    }
+}
+
+
+
+
+
 import os
 from pathlib import Path
 from dotenv import load_dotenv
