bd89f3b16af29ee599e5184024c2538b74333bd0
Fluxflare1 <fluxflareltd@gmail.com>
2024-11-03 21:02:41 +0100

    Update urls.py

--- src/backend/app/analytics/urls.py
+++ src/backend/app/analytics/urls.py
@@ -1,3 +1,20 @@
+
+
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import AnalyticsRecordViewSet
+
+router = DefaultRouter()
+router.register(r'analytics', AnalyticsRecordViewSet)
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
+
+
+
+
+
 from django.urls import path
 from .views import AnalyticsView
 
