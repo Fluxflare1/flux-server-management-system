61fca0d7ed07794b3485b9ff24856747953a00f5
Fluxflare1 <fluxflareltd@gmail.com>
2024-11-04 10:05:25 +0100

    Create UsageReport.js

---
+++ src/frontend/src/components/Reports/UsageReport.js
@@ -0,0 +1,13 @@
+// src/frontend/src/components/Reports/UsageReport.js
+import React from 'react';
+
+function UsageReport({ report }) {
+    return (
+        <div>
+            <h2>Usage Report</h2>
+            {/* Render report data */}
+        </div>
+    );
+}
+
+export default UsageReport;
