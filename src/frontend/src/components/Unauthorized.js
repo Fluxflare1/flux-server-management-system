a2c5e446fdeeeb2ec4703bb30bd3235eccc2d2c0
Fluxflare1 <fluxflareltd@gmail.com>
2024-11-06 08:10:46 +0100

    Create Unauthorized.js

---
+++ src/frontend/src/components/Unauthorized.js
@@ -0,0 +1,12 @@
+// src/frontend/components/Unauthorized.js
+
+import React from 'react';
+
+const Unauthorized = () => (
+    <div>
+        <h1>Access Denied</h1>
+        <p>You do not have permission to view this page.</p>
+    </div>
+);
+
+export default Unauthorized;
