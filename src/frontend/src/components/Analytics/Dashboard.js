39fcbbfd859490e26eac6db285bfc1725636e908
Fluxflare1 <fluxflareltd@gmail.com>
2024-11-08 10:22:59 +0100

    Create Dashboard.js

---
+++ src/frontend/src/components/Analytics/Dashboard.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function AnalyticsDashboard() {
+    return (
+        <div>
+            <h2>Analytics Dashboard</h2>
+            {/* Analytics data visualizations will be rendered here */}
+        </div>
+    );
+}
+
+export default AnalyticsDashboard;
