ebaca26fce68605b095910bb14eef1a139620520
Fluxflare1 <fluxflareltd@gmail.com>
2024-11-08 16:47:01 +0100

    Update settings_dev.py

--- config/settings_dev.py
+++ config/settings_dev.py
@@ -1,3 +1,22 @@
+
+
+# settings_dev.py
+from .base import *
+
+DEBUG = True
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+DATABASES['default'] = {
+    'ENGINE': 'django.db.backends.postgresql',
+    'NAME': 'mydb_dev',
+    'USER': 'myuser',
+    'PASSWORD': 'password',
+    'HOST': 'localhost',
+    'PORT': '5432',
+}
+
+
+
+
 # settings_dev.py
 from .base import *
 
