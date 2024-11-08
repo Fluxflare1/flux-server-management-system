5bcca5da8ee6b0d92a05891bf16345b426cbf7dd
Fluxflare1 <fluxflareltd@gmail.com>
2024-11-08 10:25:51 +0100

    Create TenantDashboard.js

---
+++ src/frontend/src/components/TenantManagement/TenantDashboard.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function TenantDashboard() {
+    return (
+        <div>
+            <h2>Tenant Management Dashboard</h2>
+            {/* Tenant information and management tools go here */}
+        </div>
+    );
+}
+
+export default TenantDashboard;
