7467a0289b1fe0150585d93f6b0b18b8a304fa1f
Fluxflare1 <fluxflareltd@gmail.com>
2024-11-08 16:45:58 +0100

    Update settings_prod.py

--- config/settings_prod.py
+++ config/settings_prod.py
@@ -1,1 +1,15 @@
 
+
+# settings_prod.py
+from .base import *
+
+DEBUG = False
+ALLOWED_HOSTS = ['your-domain.com']
+DATABASES['default'] = {
+    'ENGINE': 'django.db.backends.postgresql',
+    'NAME': 'mydb_prod',
+    'USER': 'myuser',
+    'PASSWORD': 'secure_password',
+    'HOST': 'your-database-host.com',
+    'PORT': '5432',
+}
