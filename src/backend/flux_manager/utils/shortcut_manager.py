ce2a5d552cb041d0989db4aa63c721e68a22820e
Fluxflare1 <fluxflareltd@gmail.com>
2024-11-08 16:05:08 +0100

    Create shortcut_manager.py

---
+++ src/backend/flux_manager/utils/shortcut_manager.py
@@ -0,0 +1,16 @@
+# src/backend/flux_manager/utils/shortcut_manager.py
+
+import os
+import platform
+
+def create_desktop_shortcut():
+    if platform.system() == "Windows":
+        # Windows shortcut creation
+        pass
+    elif platform.system() == "Linux":
+        # Linux shortcut creation
+        pass
+
+def create_project_folder_shortcut(project_path):
+    # Code to create shortcut for project folder
+    pass
