e42a1f1ccf03dfc9976fe06673ee1b6d453ea97f
Fluxflare1 <fluxflareltd@gmail.com>
2024-11-09 14:22:13 +0100

    Update setup.sh

--- src/scripts/setup.sh
+++ src/scripts/setup.sh
@@ -1,1 +1,16 @@
+#!/bin/bash
+# setup.sh
 
+# Variables for shortcut creation
+SERVER_MANAGER_DIR="$HOME/Desktop/FluxServerManager"
+PROJECTS_DIR="$SERVER_MANAGER_DIR/Projects"
+
+# Create directories if they don't exist
+mkdir -p "$SERVER_MANAGER_DIR"
+mkdir -p "$PROJECTS_DIR"
+
+# Create server manager shortcut
+ln -sf "$PWD/flux-server-management" "$SERVER_MANAGER_DIR/FluxServerManagerShortcut"
+
+# Provide confirmation
+echo "Server Manager and project folders shortcut created on Desktop."
