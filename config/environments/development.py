5b47f65cc83bd020c5cbe39da33dc5b9e835524c
Fluxflare1 <fluxflareltd@gmail.com>
2024-11-09 07:21:17 +0100

    Create development.py

---
+++ config/environments/development.py
@@ -0,0 +1,21 @@
+# development.py - Django specific environment configurations
+
+from .base import *
+
+DEBUG = True
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST', 'localhost'),
+        'PORT': os.getenv('DB_PORT', '5432'),
+    }
+}
+
+# Add Django Channels for real-time support if applicable
+INSTALLED_APPS += ['channels']
+ASGI_APPLICATION = 'myproject.asgi.application'
