050306edd9724163dacf55f1f91958303dec90c0
Fluxflare1 <fluxflareltd@gmail.com>
2024-11-09 09:45:58 +0100

    Update index.js

--- src/frontend/src/index.js
+++ src/frontend/src/index.js
@@ -1,1 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
 
+ReactDOM.render(<App />, document.getElementById('root'));
+
+// Register service worker for offline support
+serviceWorker.register();
