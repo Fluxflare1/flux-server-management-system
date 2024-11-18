903b629a1e1201ab10aac72a07f245191791ca9a
Fluxflare1 <fluxflareltd@gmail.com>
2024-11-18 12:58:46 +0100

    Create Billing.js

---
+++ src/frontend/src/components/CustomerDashboard/Billing.js
@@ -0,0 +1,14 @@
+// Path: frontend/src/components/CustomerDashboard/Billing.js
+import React from 'react';
+import ContextualHelp from '../Help/ContextualHelp';
+
+const Billing = () => {
+  return (
+    <div>
+      <h3>Billing</h3>
+      <ContextualHelp url="https://docs.flux.com/billing" text="Learn about billing" />
+    </div>
+  );
+};
+
+export default Billing;
