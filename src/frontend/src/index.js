12ae2e52514f88464ae39ade97c0e8150dcfda53
Itebimien Momodu <fluxflareltd@gmail.com>
2024-12-20 10:16:11 +0100

    Update index.js

--- src/frontend/src/index.js
+++ src/frontend/src/index.js
@@ -1,3 +1,16 @@
+
+
+import * as Sentry from "@sentry/react";
+import { Integrations } from "@sentry/tracing";
+
+Sentry.init({
+    dsn: "https://<your-sentry-dsn>",
+    integrations: [new Integrations.BrowserTracing()],
+    tracesSampleRate: 1.0,
+});
+
+
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
