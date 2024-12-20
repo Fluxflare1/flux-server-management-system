5dbabddafec0caa16ba8cba88ca61e972cab85d5
Itebimien Momodu <fluxflareltd@gmail.com>
2024-12-20 10:38:24 +0100

    Update models.py

--- src/backend/app/models.py
+++ src/backend/app/models.py
@@ -1,3 +1,21 @@
+
+from django.db import models
+from django.contrib.auth.models import User
+
+class Notification(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    message = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    is_read = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.title} - {self.user.username}"
+
+
+
+
+
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
