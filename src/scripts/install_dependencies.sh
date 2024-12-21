58990e4934f9b030d0aad73bf4aa50c9001b9e8e
Itebimien Momodu <fluxflareltd@gmail.com>
2024-12-21 09:57:00 +0100

    Update install_dependencies.sh

--- src/scripts/install_dependencies.sh
+++ src/scripts/install_dependencies.sh
@@ -1,3 +1,22 @@
+
+
+#!/bin/bash
+echo "Installing Dependencies..."
+
+if [ -f "src/backend/requirements.txt" ]; then
+  pip install -r src/backend/requirements.txt
+elif [ -f "src/frontend/package.json" ]; then
+  yarn install --cwd src/frontend/
+elif [ -f "src/ruby-app/Gemfile" ]; then
+  bundle install --gemfile=src/ruby-app/Gemfile
+elif [ -f "src/go-app/go.mod" ]; then
+  cd src/go-app && go mod tidy
+else
+  echo "No dependencies to install."
+fi
+
+
+
 #!/bin/bash
 # Script for installing dependencies automatically for Django project
 
